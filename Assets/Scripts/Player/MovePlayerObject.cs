c41a72980db504019b1fd9f650c576ffccd92bf0
jinxie88 <jinxie88@live.nl>
2014-02-18 14:31:18 +0100

    tweaking kaas kaas kaas kaas
    kaas kaas kaas kaas kaas kaas kaas kaas kaas

---
+++ Assets/Scripts/Player/MovePlayerObject.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayerObject : MonoBehaviour {
+	public static bool rightMovement = false;
+	public static bool leftMovement = false;
+	public static bool jump = false;
+	public static bool standingOnGround = true;
+
+	void Update()
+	{
+		if(rightMovement)
+		{
+			transform.position += new Vector3(0.06f,0,0);
+		}else if(leftMovement)
+		{
+			transform.position += new Vector3(-0.06f,0,0);
+		}
+		if(jump && standingOnGround)
+		{
+			rigidbody.AddForce(0, 500, 0);
+			//rigidbody.AddRelativeForce(Vector3.up * 8000 * (Time.deltaTime));
+			jump = false;
+			standingOnGround = false;
+		}
+		Deactivate();
+	}
+	void Deactivate()
+	{
+		rightMovement = false;
+		leftMovement = false;
+	}
+	void OnCollisionEnter(Collision col)
+	{
+		if(col.collider.tag == "Ground")
+		{
+			standingOnGround = true;
+		}
+
+		if (col.collider.name == "End")
+		{
+			Application.LoadLevel(3);
+		}
+
+		if(col.collider.name == "KillingObjects")
+		{
+			Destroy(col.collider.gameObject);
+			Application.LoadLevel(2);
+		}	
+	}
+
+	/*void OnTriggerEnter(Collider col)
+	{
+		if (col.collider.name == "Gold")
+		{
+			Destroy(gameObject);
+		}
+	}*/
+
+}
+
