a5bcdba74d9fe854a19c0bb244145e2c93051998
jeremybond <jeremybond2@hotmail.com>
2014-02-19 11:45:57 +0100

    cameraHalfDone
    the camera movement is still not entirly goood, but it's half oke

--- Assets/Scripts/MovePlayerObject.cs
+++ Assets/Scripts/MovePlayerObject.cs
@@ -4,33 +4,25 @@ using System.Collections;
 public class MovePlayerObject : MonoBehaviour {
 	public static bool rightMovement = false;
 	public static bool leftMovement = false;
-	public static bool jump = false;
 	public static bool standingOnGround = true;
+	public static bool jump = false;
 
 	void Update()
 	{
 		if(rightMovement){
-			transform.position += new Vector3(0.5f,0,0);
+			transform.position += new Vector3(0.2f,0,0);
 		}else if(leftMovement){
-			transform.position += new Vector3(-0.5f,0,0);
+			transform.position += new Vector3(-0.2f,0,0);
 		}
-		if(jump && standingOnGround){
-			//rigidbody.AddForce(0, 55, 0);
-			rigidbody.AddRelativeForce(Vector3.up * 2500 * (Time.deltaTime ));
-			jump = false;
-			standingOnGround = false;
+		if(jump ){
+			rigidbody.AddRelativeForce(Vector3.up * 7000 * (Time.deltaTime ));
+			LeftEnRightButton.jumpCapability = false;
 		}
 		Deactivate();
 	}
 	void Deactivate(){
 		rightMovement = false;
 		leftMovement = false;
-	}
-	void OnCollisionEnter(Collision col){
-		if(col.collider.tag == "Ground")
-		{
-			standingOnGround = true;
-		}
 	}
 }
 
